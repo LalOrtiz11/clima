40b938a254870687723bd4dee19c36826910baa0
LalOrtiz11 <eduardo_ortizm@outlook.com>
2023-09-02 17:50:10 -0600

    se hizo la instancia y se conecto al archivo

--- 01.counter/src/App.js
+++ 01.counter/src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Counter from './counter';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        
+        <Counter initCount={52}/>
       </header>
     </div>
   );
